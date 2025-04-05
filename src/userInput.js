9954d9b135ea2b8ebba1603cccd826b6c4e7b19d
TheHarshKadam <harshvardhankadam89@gmail.com>
2025-04-06 00:35:09 +0530

    created userInput component

---
+++ src/userInput.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React, { useState } from 'react';
+
+const UserInput = () => {
+    const [input, setInput] = useState('');
+  return (
+    <div>
+        <h1> User Input</h1>
+        <input type="text" placeholder='Type anything' onChange={(e)=>setInput(e.target.value)}/>
+        <p>Typed String : {input}</p>
+    </div>
+  )
+}
+
+export default UserInput;
